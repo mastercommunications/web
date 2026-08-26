c29e21c9a0777290af4566bab12cb60733ca8419
mastercommunications <mastercommunicationsgithub@gmail.com>
2026-08-26 15:25:57 +0000

    fklfskjskfskjxklcjdkljdljf,xjklxnxklfjpivjx;fjio

--- script.js
+++ script.js
@@ -2,6 +2,36 @@ const menuToggle = document.querySelector('.menu-toggle');
 const nav = document.querySelector('.site-nav');
 const modal = document.querySelector('.modal');
 
+const projectPages = {
+  'IMG-20260826-WA0025.jpg': 'omar.html',
+  'IMG-20260826-WA0023.jpg': 'featured-natok.html',
+  'IMG-20260826-WA0027.jpg': 'boro-bhai.html',
+  'IMG-20260826-WA0026.jpg': 'original-natok.html',
+  'IMG-20260826-WA0028.jpg': 'danger-bou.html',
+  'IMG-20260826-WA0030.jpg': 'omar.html',
+  'IMG-20260826-WA0038.jpg': 'omar.html'
+};
+
+if (window.location.pathname.endsWith('/omar.html')) {
+  const videoPlaceholder = document.querySelector('.video-placeholder');
+  if (videoPlaceholder) {
+    videoPlaceholder.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/kHDkglPtT5A?si=aEcO_FSwW2SW_qtc" title="OMAR film video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
+  }
+}
+
+document.querySelectorAll('.project, .archive-card').forEach((project) => {
+  const image = project.querySelector('img');
+  const fileName = image?.src.split('/').pop();
+  const detailPage = projectPages[fileName];
+  if (detailPage) {
+    project.addEventListener('click', (event) => {
+      if (event.target.closest('[data-reel]')) return;
+      window.location.href = detailPage;
+    });
+    project.style.cursor = 'pointer';
+  }
+});
+
 menuToggle.addEventListener('click', () => {
   const isOpen = nav.classList.toggle('open');
   menuToggle.setAttribute('aria-expanded', isOpen);
